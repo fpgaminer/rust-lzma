f939b8bc7577781f946bb2f70fb1fabe512eedcf
Kartik Sharma <kartik.sharma522@gmail.com>
2023-04-08 21:54:07 +0530

    Add support for vcpkg for linking on msvc

--- build.rs
+++ build.rs
@@ -1,12 +1,24 @@
 use std::env;
 
 fn main() {
+    let target = env::var("TARGET").unwrap();
+    let is_windows = target.contains("windows");
     let statik = env::var("CARGO_FEATURE_STATIC").is_ok();
 
-    pkg_config::Config::new()
-        .statik(statik)
-        .probe("liblzma")
-        .expect("Could not find liblzma using pkg-config");
+    if is_windows {
+        #[cfg(windows)]
+        vcpkg::Config::new()
+            .emit_includes(true)
+            .find_package("liblzma")
+            .expect("Could not find liblzma using vcpkg");
+    } else {
+        #[cfg(unix)]
+        pkg_config::Config::new()
+            .statik(statik)
+            .probe("liblzma")
+            .expect("Could not find liblzma using pkg-config");
+    }
+
     if statik {
         println!("cargo:rustc-link-lib=static=lzma");
     }
